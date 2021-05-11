adcf3c0302c1e34cd9791c4fde315b709d5df15f
Rayane'CO <81171554+Rayaneko@users.noreply.github.com>
2021-05-11 15:37:25 +0200

    Add files via upload

---
+++ exercice-react-master/src/App.js
@@ -0,0 +1,12 @@
+import UserCard from './UserCard'
+
+const App = () => {
+  return (
+    <>
+      <h1>Exercices React</h1>
+      <UserCard />
+    </>
+  )
+}
+
+export default App
---
+++ exercice-react-master/src/UserCard.js
@@ -0,0 +1,19 @@
+const Avatar = (props) => {
+	return (
+		<img src="" alt="Profil of" />
+	)
+}
+
+const UserCard = (props) => {
+	return (
+		<div className="card">
+			<Avatar />
+			<div>
+				<p>Prénom: </p>
+				<p>House: </p>
+			</div>
+		</div>
+	)
+}
+
+export default UserCard
